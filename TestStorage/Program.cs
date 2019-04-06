ed0ec54bb5f9aa0221ac515292f22a7bdfed79bb
novakvova <novakvova@gmail.com>
2019-04-06 13:20:02 +0300

    Test Storage Procedure

--- TestStorage/Program.cs
+++ TestStorage/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 using TestStorage.Entities;
 using System.Data.Entity;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json;
+using TestStorage.ViewModel;
 
 namespace TestStorage
 {
@@ -19,40 +23,91 @@ namespace TestStorage
                 //Console.WriteLine("Begin Seed");
                 //SeedDB(context);
                 //Console.WriteLine("End Seed");
-                Stopwatch stopWatch = new Stopwatch();
-                stopWatch.Start();
-                Console.WriteLine("Begin red");
-                var query = context.Users
-                    .Include(u => u.UserImages)
-                    .Select(u=>new
-                    {
-                        u.Id,
-                        u.FirstName,
-                        u.LastName,
-                        u.Sex,
-                        u.Email,
-                        u.Age,
-                        Images = u.UserImages.Select(g=>g)
-                    });
-                query = query.OrderBy(u => u.Id).Skip(10).Take(20000);
-                int i = 1;
-                foreach (var user in query)
-                {
-                    var data = user;
-                    //Console.WriteLine("Count: {0}", i);
-                    i++;
-                }
-                Console.WriteLine("End read: {0}",i);
-                stopWatch.Stop();
-                // Get the elapsed time as a TimeSpan value.
-                TimeSpan ts = stopWatch.Elapsed;
+                //ReadDataEFContext(context);
 
-                // Format and display the TimeSpan value.
-                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    ts.Hours, ts.Minutes, ts.Seconds,
-                    ts.Milliseconds / 10);
-                Console.WriteLine("RunTime " + elapsedTime);
+                Console.WriteLine("---------Read EFContext-----------");
+                ReadDataEFContext(context, 0, 50000);
+                Console.WriteLine("---------Read Storage-----------");
+                GetDataSQLStorage(context,20000,50000);
+                Console.WriteLine("---------Read EFContext-----------");
+                ReadDataEFContext(context, 60000, 50000);
+                Console.WriteLine("---------Read Storage-----------");
+                GetDataSQLStorage(context, 0, 50000);
+                Console.WriteLine("---------Read EFContext-----------");
+                ReadDataEFContext(context, 10000, 50000);
+                Console.WriteLine("---------Read Storage-----------");
+                GetDataSQLStorage(context, 10000, 50000);
             }
+        }
+        static void GetDataSQLStorage(EFContext context, int skip, int take)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("Begin red");
+            var jsonOut = new SqlParameter();
+            jsonOut.ParameterName = "@JSONOutput";
+            jsonOut.Direction = ParameterDirection.Output;
+            jsonOut.SqlDbType = SqlDbType.NVarChar;
+            jsonOut.Size = -1;
+            string sql = @"EXEC [dbo].[spGetRangeUers]
+                                @From,
+                        		@Take,
+                                @JSONOutput out";
+            context.Database.ExecuteSqlCommand(sql,
+                new SqlParameter("@From", Convert.ToInt32(0)),
+                new SqlParameter("@Take", 100),
+                jsonOut);
+            var data = JsonConvert.DeserializeObject<List<UserViewModel>>(jsonOut.Value.ToString());
+            //Console.WriteLine(jsonOut.Value);
+            Console.WriteLine("End read: ");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds );
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+        static void ReadDataEFContext(EFContext context, int skip, int take)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("Begin red");
+            var query = context.Users
+                .Include(u => u.UserImages)
+                .Select(u => new UserViewModel
+                {
+                    UserId= u.Id,
+                    UserName= u.FirstName,
+                    UserSex= u.Sex,
+                    UserAge= u.Age,
+                    UserEmail= u.Email,
+                    Images = u.UserImages.Select(g => new UserImageViewModel
+                    {
+                        Id=g.Id,
+                        Name=g.Name
+                    }).ToList()
+                });
+            query = query.OrderBy(u => u.UserId).Skip(10).Take(20000);
+            int i = 1;
+            foreach (var user in query)
+            {
+                var data = user;
+                //Console.WriteLine("Count: {0}", i);
+                i++;
+            }
+            Console.WriteLine("End read: {0}", i);
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds);
+            Console.WriteLine("RunTime " + elapsedTime);
         }
         static void SeedDB(EFContext context)
         {
---
+++ TestStorage/ViewModel/UserViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStorage.ViewModel
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public bool UserSex { get; set; }
+        public string UserEmail { get; set; }
+        public int UserAge { get; set; }
+        public List<UserImageViewModel> Images { get; set; }
+    }
+
+    public class UserImageViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
