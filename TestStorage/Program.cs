bec3061d3075c5e681a392daa4b71db30654fcce
novakvova <novakvova@gmail.com>
2019-04-06 11:49:26 +0300

    Read Data in Speed

--- TestStorage/Program.cs
+++ TestStorage/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestStorage.Entities;
+using System.Data.Entity;
+using System.Diagnostics;
 
 namespace TestStorage
 {
@@ -14,9 +16,41 @@ namespace TestStorage
         {
             using (EFContext context = new EFContext())
             {
-                Console.WriteLine("Begin Seed");
-                SeedDB(context);
-                Console.WriteLine("End Seed");
+                //Console.WriteLine("Begin Seed");
+                //SeedDB(context);
+                //Console.WriteLine("End Seed");
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Console.WriteLine("Begin red");
+                var query = context.Users
+                    .Include(u => u.UserImages)
+                    .Select(u=>new
+                    {
+                        u.Id,
+                        u.FirstName,
+                        u.LastName,
+                        u.Sex,
+                        u.Email,
+                        u.Age,
+                        Images = u.UserImages.Select(g=>g)
+                    });
+                int i = 1;
+                foreach (var user in query)
+                {
+                    var data = user;
+                    //Console.WriteLine("Count: {0}", i);
+                    i++;
+                }
+                Console.WriteLine("End read: {0}",i);
+                stopWatch.Stop();
+                // Get the elapsed time as a TimeSpan value.
+                TimeSpan ts = stopWatch.Elapsed;
+
+                // Format and display the TimeSpan value.
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                Console.WriteLine("RunTime " + elapsedTime);
             }
         }
         static void SeedDB(EFContext context)
